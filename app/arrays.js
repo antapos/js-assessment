6a0702ad889d43cd20a1ba23e0c36328dc1482b1
Antonis Apostolakis <apostolakis.antonis@gmail.com>
2013-09-04 17:43:48 +0300

    Passed tests for bestPractices, modules, arrays, objects, flowControl

--- app/arrays.js
+++ app/arrays.js
@@ -1,61 +1,106 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+  var define = require('amdefine')(module);
+}
 
-define(function() {
+define(function () {
   return {
-    indexOf : function(arr, item) {
-
+    indexOf: function (arr, item) {
+      return arr.indexOf(item);
     },
 
-    sum : function(arr) {
-
+    sum: function (arr) {
+      var total = 0;
+      arr.forEach(function (number) {
+        total += number;
+      });
+      return total;
     },
 
-    remove : function(arr, item) {
-
+    remove: function (arr, item) {
+      var copy = [];
+      arr.forEach(function (el) {
+        if (el !== item) {
+          copy.push(el);
+        }
+      });
+      return copy;
     },
 
-    removeWithoutCopy : function(arr, item) {
-
+    removeWithoutCopy: function (arr, item) {
+      var i;
+      while ((i = arr.indexOf(item)) > 0) {
+        arr.splice(i, 1);
+      }
+      return arr;
     },
 
-    append : function(arr, item) {
-
+    append: function (arr, item) {
+      arr.push(item);
+      return arr;
     },
 
-    truncate : function(arr) {
-
+    truncate: function (arr) {
+      arr.pop();
+      return arr;
     },
 
-    prepend : function(arr, item) {
-
+    prepend: function (arr, item) {
+      arr.unshift(item);
+      return arr;
     },
 
-    curtail : function(arr) {
-
+    curtail: function (arr) {
+      arr.shift(arr);
+      return arr;
     },
 
-    concat : function(arr1, arr2) {
-
+    concat: function (arr1, arr2) {
+      return arr1.concat(arr2);
     },
 
-    insert : function(arr, item, index) {
-
+    insert: function (arr, item, index) {
+      arr.splice(index, 0, item);
+      return arr;
     },
 
-    count : function(arr, item) {
-
+    count: function (arr, item) {
+      var count = 0;
+      arr.forEach(function (el) {
+        if (el === item) {
+          count++;
+        }
+      });
+      return count;
     },
 
-    duplicates : function(arr) {
-
+    duplicates: function (arr) {
+      var explored = {};
+      var duplis = [];
+      arr.forEach(function (el) {
+        if (explored.hasOwnProperty(el) && explored[el] === 1) {
+          duplis.push(el);
+          explored[el] += 1;
+        } else {
+          explored[el] = 1;
+        }
+      });
+      return duplis;
     },
 
-    square : function(arr) {
-
+    square: function (arr) {
+      return arr.map(function (el) {
+        return el * el;
+      });
     },
 
-    findAllOccurrences : function(arr, target) {
-
+    findAllOccurrences: function (arr, target) {
+      var result = [];
+      for(var i=0; i<arr.length; i++) {
+        if (arr[i] === target) {
+          result.push(i);
+        }
+      }
+      return result;
     }
   };
 });
