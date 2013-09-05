92c43afd08620d8128407ca0db5a4092d3fa4d9d
Antonis Apostolakis <apostolakis.antonis@gmail.com>
2013-09-05 18:27:26 +0300

    - Passed regex, recursion and logicalOperators tests

--- app/recursion.js
+++ app/recursion.js
@@ -3,11 +3,40 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
-
+      var files = [];
+      function scan(folder, filter) {
+        folder.files.forEach(function(fileOrDir) {
+          if (!filter || fileOrDir.dir === filter) {
+            if (typeof fileOrDir === 'object') {
+              scan(fileOrDir);
+            } else {
+              files.push(fileOrDir);
+            }
+          }
+        });
+      }
+      scan(data, dirName);
+      return files;
     },
 
     permute: function(arr) {
-
+      var result = [],
+        explored = [];
+      function accumulate(input) {
+        var i, ch;
+        for (i=0; i<input.length; i++) {
+          ch = input.splice(i, 1)[0];
+          explored.push(ch);
+          if (input.length === 0) {
+            result.push(explored.slice());
+          }
+          accumulate(input);
+          input.splice(i, 0, ch);
+          explored.pop();
+        }
+        return result;
+      }
+      return accumulate(arr);
     }
   };
 });
--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,28 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+      return !!str.match(/\d/);
     },
 
     containsRepeatingLetter : function(str) {
-
+      return !!str.match(/([a-zA-Z])\1/g);
     },
 
     endsWithVowel : function(str) {
-
+      return !!str.match(/[aeiouAEIOU]$/);
     },
 
     captureThreeNumbers : function(str) {
-
+      var found = str.match(/\d{3}/);
+      return found ?  found[0] : false;
     },
 
     matchesPattern : function(str) {
-
+      return !!str.match(/^\d{3}\-\d{3}\-\d{4}$/);
     },
-    isUSD : function(str) {
 
+    isUSD : function(str) {
+      return !!str.match(/^\$\d+([,]\d{3})*([.]\d{2})?$/);
     }
   };
 });
