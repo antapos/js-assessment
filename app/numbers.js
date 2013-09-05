a238514cb515a4e582f2589b90e47aab783af49f
Antonis Apostolakis <apostolakis.antonis@gmail.com>
2013-09-05 19:17:11 +0300

    - Passed numbers tests

--- app/numbers.js
+++ app/numbers.js
@@ -3,19 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+      return (num & (1 << (bit - 1))) >> (bit - 1);
     },
 
     base10: function(str) {
-
+      return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
-
+      return ('00000000' + (num).toString(2)).substr(-8);
     },
 
     multiply: function(a, b) {
-
+      //TODO: find decimal places to find multiplier/divider for each operand
+      var multiplier = 10000;
+      return (a*(b*multiplier))/multiplier;
     }
   };
 });
