6f8ee0b8832835e5e88121b086040d4861843062
Antonis Apostolakis <apostolakis.antonis@gmail.com>
2013-09-06 11:23:33 +0300

    - Passed async and count tests

--- app/async.js
+++ app/async.js
@@ -3,11 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+      var deferred = new $.Deferred();
+      deferred.resolve(value);
+      return deferred.promise();
     },
 
     manipulateRemoteData : function(url) {
-
+      var deferred = new $.Deferred();
+      $.ajax(url).then(function(response) {
+        var names = response.people.map(function(obj) {
+          return obj.name;
+        });
+        names.sort();
+        deferred.resolve(names);
+      });
+      return deferred.promise();
     }
   };
 });
--- app/count.js
+++ app/count.js
@@ -3,6 +3,25 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var counter = {
+        cancelRequested: false,
+        cancel: function() {
+          counter.cancelRequested = true;
+        },
+        iterate: function(current, end) {
+          if (counter.cancelRequested) {
+            return;
+          }
+          console.log(current);
+          if (current < end) {
+            setTimeout(function() {
+              counter.iterate(current+1, end);
+            }, 100);
+          }
+        }
+      };
+      counter.iterate(start, end);
+      return counter;
     }
   };
 });
